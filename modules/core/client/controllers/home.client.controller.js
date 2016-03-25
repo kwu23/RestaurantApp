d0f5c8065d70ac273208190b5a6567b460661c1c
Kevin <kev.k.wu@gmail.com>
2016-03-24 23:31:09 -0400

    XD

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,17 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    $scope.quantity = 1;
+    $scope.pricePho = 12;
+    $scope.addPho = function(){
+        $scope.quantity = $scope.quantity+1;
+        $scope.pricePho = $scope.pricePho + 12;
+    };
+    $scope.subtractPho = function(){
+        if($scope.quantity !== 0){
+            $scope.quantity = $scope.quantity-1;
+            $scope.pricePho = $scope.pricePho - 12;
+        }
+    };
   }
 ]);
